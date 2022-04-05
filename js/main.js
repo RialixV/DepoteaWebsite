8001adf34a54ba3f1546fdba45f782035677288e
Ezgord <75807910+Ezgord@users.noreply.github.com>
2022-04-05 17:42:22 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,8 +2,49 @@
 //HAMBURGER MENU TOGGLER
 
 $(document).ready(function () {
-	$('#toggle').click(function() {
-		$(this).toggleClass('active');
+	$('.navbarMenuToggle').click(function() {
+		$('.fullnavBtn').toggleClass('active');
 		$('#fullnav').toggleClass('open');
 	});
 });
+
+//AJAX PAGE LOADER
+
+function loadMainpage() {
+	var xhttp = new XMLHttpRequest();
+	
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("index-body-container").innerHTML = this.responseText;
+		}
+	};
+	
+	xhttp.open("GET", "mainpage.html", true);
+	xhttp.send();
+}
+
+function loadBeverage() {
+	var xhttp = new XMLHttpRequest();
+	
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("index-body-container").innerHTML = this.responseText;
+		}
+	};
+	
+	xhttp.open("GET", "menu.html", true);
+	xhttp.send();
+}
+
+function loadFood() {
+	var xhttp = new XMLHttpRequest();
+	
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("index-body-container").innerHTML = this.responseText;
+		}
+	};
+	
+	xhttp.open("GET", "menu.html", true);
+	xhttp.send();
+}
