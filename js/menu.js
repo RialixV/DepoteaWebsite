ac05dd9a23a16ef1f93e475de422db2b68d4a60d
RialixV <70130482+RialixV@users.noreply.github.com>
2022-04-06 12:04:24 +0800

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -51,60 +51,159 @@ function modalChange(event) {
     console.log(titleElement)
 }
 
+var lastSize = 70
+var lastAddon = 0
+
 function ItemSizePriceUpdate(clicked_id){
-    var ItemsizeLarge = document.getElementById('flexRadioDefault8')
-    var modalParent = ItemsizeLarge.parentElement.parentElement.parentElement.parentElement
     var ActiveID = clicked_id
     console.log(ActiveID)
     switch (ActiveID){
-        case "flexRadioDefault7":
-        modalParent.getElementsByClassName('shop-item-modal-price')[0].innerText = '₱' + '70'
-        break;
-        case "flexRadioDefault8":
-        modalParent.getElementsByClassName('shop-item-modal-price')[0].innerText = '₱' + '80'
-        break;
-    }
+		case "flexRadioDefault7":
+			switch (lastAddon){
+				case 0:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱70"
+					lastSize = 70
+					break;
+				case 10:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱80"
+					lastSize = 70
+					break;
+				case 15:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱85"
+					lastSize = 70
+					break;
+				case 20:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱90"
+					lastSize = 70
+					break;
+				case 30:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱100"
+					lastSize = 70
+					break;
+			}
+			break;
+		case "flexRadioDefault8":
+			switch (lastAddon){
+				case 0:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱80"
+					lastSize = 80
+					break;
+				case 10:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱90"
+					lastsize = 80
+					break;
+				case 15:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱95"
+					lastSize = 80
+					break;
+				case 20:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱100"
+					lastsize = 80
+					break;
+				case 30:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱110"
+					lastsize = 80
+					break;
+			}
+			break;
+	}	
 }
 
-function addOnPriceUpdate(clicked_id){
-    var sinkersElement = document.getElementById('flexRadioDefault1')
-    var modalParent = sinkersElement.parentElement.parentElement.parentElement.parentElement
-    var ActiveId = clicked_id
-    var itemprice = modalParent.getElementsByClassName('shop-item-modal-price')[0].innerText
-    var price = parseFloat(itemprice.replace('₱', ''))
-    console.log(price)
-    switch (ActiveId){
+function addOnPriceUpdate(clicked_id) {
+	var addonIdClicked = clicked_id
+    console.log(addonIdClicked)
+	switch (addonIdClicked){
+		case "flexRadioDefault":
+			switch (lastSize){
+				case 70:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱70"
+					lastAddon = 0
+					break;
+				case 80:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱80"
+					lastAddon = 0
+					break;
+			}
+            break;
         case "flexRadioDefault1":
-            var totalprice = price + 20
-            modalParent.getElementsByClassName('shop-item-modal-price')[0].innerText = '₱' + totalprice
+			switch (lastSize){
+				case 70:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱90"
+					lastAddon = 20
+					break;
+				case 80:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱100"
+					lastAddon = 20
+					break;
+			}
             break;
-            case "flexRadioDefault2":
-            totalprice = price + 30
-            modalParent.getElementsByClassName('shop-item-modal-price')[0].innerText = '₱' + totalprice 
+		case "flexRadioDefault2":
+			switch (lastSize){
+				case 70:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱100"
+					lastAddon = 30
+					break;
+				case 80:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱110"
+					lastAddon = 30
+					break;
+			}
             break;
-            case "flexRadioDefault3":
-            totalprice = price + 2
-            modalParent.getElementsByClassName('shop-item-modal-price')[0].innerText = '₱' + totalprice 
+		case "flexRadioDefault3":
+			switch (lastSize){
+				case 70:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱90"
+					lastAddon = 20
+					break;
+				case 80:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱100"
+					lastAddon = 20
+					break;
+			}
             break;
-            case "flexRadioDefault4":
-            totalprice = price
-            modalParent.getElementsByClassName('shop-item-modal-price')[0].innerText = '₱' + totalprice 
+		case "flexRadioDefault4":
+			switch (lastSize){
+				case 70:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱85"
+					lastAddon = 15
+					break;
+				case 80:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱95"
+					lastAddon = 15
+					break;
+			}
             break;
-            case "flexRadioDefault5":
-            totalprice = price
-            modalParent.getElementsByClassName('shop-item-modal-price')[0].innerText = '₱' + totalprice
+		case "flexRadioDefault5":
+			switch (lastSize){
+				case 70:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱85"
+					lastAddon = 15
+					break;
+				case 80:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱95"
+					lastAddon = 15
+					break;
+			}
             break;
-            case "flexRadioDefault6":
-            totalprice = price
-            modalParent.getElementsByClassName('shop-item-modal-price')[0].innerText = '₱' + totalprice
+		case "flexRadioDefault6":
+			switch (lastSize){
+				case 70:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱80"
+					lastAddon = 10
+					break;
+				case 80:
+					document.getElementsByClassName('shop-item-modal-price')[0].innerText = "₱90"
+					lastAddon = 10
+					break;
+			}
             break;
     }
-
 }
 
 function resetModal(){
     $('#exampleModal').on('hidden.bs.modal', function () {
         $(this).find('form').trigger('reset');
-
+        lastSize = 70
+        lastAddon = 0
     })
 }
